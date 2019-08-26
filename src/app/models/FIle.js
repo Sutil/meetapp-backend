69af390759a815734df67574b9a3615d380bfb3a
Eduardo <sutil.edu@gmail.com>
2019-08-26 08:52:08 -0300

    Criando arquivo File.js

---
+++ src/app/models/FIle.js
@@ -0,0 +1,23 @@
+import { Model, Sequelize } from 'sequelize';
+
+class File extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        path: Sequelize.BOOLEAN,
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `${process.env.APP_URL}/files/${this.path}`;
+          },
+        },
+      },
+      sequelize
+    );
+
+    return this;
+  }
+}
+
+export default File;
